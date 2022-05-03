59bc3c00b5f6d7eb5da47dea255476647ebe531b
matheussilvino <81540139+matheussilvino@users.noreply.github.com>
2022-05-03 12:48:24 -0300

    Add files via upload

---
+++ src/Button.style.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  buttonStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 1,
+    borderRadius: 50,
+    elevation: 3,
+    backgroundColor: '#6974CF',
+    marginLeft: 50,
+    marginRight: 50,
+    marginTop: 130,
+  },
+  buttonText: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+});
+
