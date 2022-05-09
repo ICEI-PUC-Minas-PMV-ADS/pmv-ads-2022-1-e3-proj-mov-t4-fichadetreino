b0ccef366625ab6f59df56da02e30c9f2e127272
matheussilvino <81540139+matheussilvino@users.noreply.github.com>
2022-05-08 22:07:36 -0300

    Entrega etapa 3

--- src/README.md
+++
@@ -1,1 +0,0 @@
-O projeto em si esta aqui.
--- src/services/urls.js
+++
@@ -1,2 +0,0 @@
-//export const BASE_URL = 'https://fake-api-json-server-recrutati.herokuapp.com';
-export const BASE_URL = 'https://sixty-carpets-change-186-206-255-48.loca.lt';
